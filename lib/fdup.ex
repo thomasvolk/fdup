5cf79a4115c2abd443bf611a434ba401f2a3382e
Thomas Volk <info@thomasvolk.de>
2017-09-20 23:19:17 +0200

    add fdup test

--- lib/fdup.ex
+++ lib/fdup.ex
@@ -17,7 +17,7 @@ defmodule FDup do
   limitations under the License.
   """
   def main(args) do
-    args |> parse_args |> process
+    args |> parse_args |> process(&IO.puts/1)
   end
   alias FDup.Directory, as: Directory
   alias FDup.DB, as: DB
@@ -30,40 +30,40 @@ defmodule FDup do
     System.halt(1)
   end
 
-  def process(%{options: _, args: []}), do: usage()
+  def process(%{options: _, args: []}, _), do: usage()
 
-  def process(%{options: [], args: [path]}) do
-    process([], "duplicate", path)
+  def process(%{options: [], args: [path]}, print) do
+    process([], "duplicate", path, print)
   end
 
-  def process(%{options: [{:mode, mode}|options], args: [path]}) do
-    process(options, mode, path)
+  def process(%{options: [{:mode, mode}|options], args: [path]}, print) do
+    process(options, mode, path, print)
   end
 
-  def process(options, mode, path) do
+  def process(options, mode, path, print) do
     Logger.debug "create index path=#{path}"
     db = Directory.traverse([path], &DB.update_from_file/2, DB.new)
     Logger.debug "generate report mode=#{mode}"
-    report(options, String.to_atom(mode), db)
+    report(options, String.to_atom(mode), db, print)
     Logger.debug "done"
   end
 
-  def report([{:group, level}|_], :unique, db) do
+  def report([{:group, level}|_], :unique, db, print) do
     %{duplicates: _, uniques: uniques} = DB.groups(db, elem(Integer.parse(level), 0))
-    Enum.each(Group.counts(uniques), fn [p, c] -> IO.puts "#{c} #{p}" end)
+    Enum.each(Group.counts(uniques), fn [p, c] -> print.("#{c} #{p}") end)
   end
 
-  def report([], :unique, db) do
-    Enum.each(DB.unique_entries(db), &IO.puts/1)
+  def report([], :unique, db, print) do
+    Enum.each(DB.unique_entries(db), print)
   end
 
-  def report([{:group, level}], :duplicate, db) do
+  def report([{:group, level}], :duplicate, db, print) do
     %{duplicates: duplicates, uniques: _} = DB.groups(db, elem(Integer.parse(level), 0))
-    Enum.each(Group.counts(duplicates), fn [p, c] -> IO.puts "#{c} #{p}" end)
+    Enum.each(Group.counts(duplicates), fn [p, c] -> print.("#{c} #{p}") end)
   end
 
-  def report([], :duplicate, db) do
-    Enum.each(DB.duplicate_entries(db), &IO.puts/1)
+  def report([], :duplicate, db, print) do
+    Enum.each(DB.duplicate_entries(db), print)
   end
 
   defp parse_args(args) do
--- test/fdup_test.exs
+++ test/fdup_test.exs
@@ -3,6 +3,7 @@ defmodule FDupTest do
   doctest FDup
 
   test "fdup" do
-
+    path = "test_data"
+    FDup.process(%{options: [{:mode, "unique"}], args: [path]}, fn s -> s end)
   end
 end
